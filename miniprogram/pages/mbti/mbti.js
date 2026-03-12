ac5fe605ea46b20283e676fd5a964f77eefdcd41
yudejia <yudejia@meishubao.com>
2026-03-12 21:30:40 +0800

    feat: introduce MBTI personality lab page embedding an H5 application.

---
+++ miniprogram/pages/mbti/mbti.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+    // Because we are using localhost to debug, make sure the local server is running.
+    // E.g. run `python3 -m http.server 8080` in `h5-mbti/` directory.
+    url: 'http://127.0.0.1:8080'
+  },
+  onLoad: function (options) {
+    // Do any initialization here
+  }
+})
